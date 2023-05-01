90606841389638ca3a60e7c2cbd40959ecf46e77
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-05-01 10:32:02 +0300

    İnitial commit

---
+++ src/test/java/com/blog/webService/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.webService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
