78fbfa2267e5345c34df364f37bb2d2faf1b6b94
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-05-02 11:56:56 +0300

    h2 connect

--- src/main/java/com/blog/webService/user/User.java
+++ src/main/java/com/blog/webService/user/User.java
@@ -1,9 +1,18 @@
 package com.blog.webService.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue
+    private long id;
     private String username;
     private String email;
     private String password;
