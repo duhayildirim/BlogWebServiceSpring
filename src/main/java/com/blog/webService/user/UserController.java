62512c1892304a22434fed005a59313af1b855ef
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-05-02 11:06:37 +0300

    Cross

--- src/main/java/com/blog/webService/user/UserController.java
+++ src/main/java/com/blog/webService/user/UserController.java
@@ -2,6 +2,7 @@ package com.blog.webService.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    @CrossOrigin
     @PostMapping("/api/1.0/users")
     public void createUser(@RequestBody User user) {
         log.info(user.toString());
