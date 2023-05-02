369ff9de1e54222cfe0767a4030f0737c4aa77b2
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-05-02 13:39:44 +0300

    Status message

--- src/main/java/com/blog/webService/user/UserController.java
+++ src/main/java/com/blog/webService/user/UserController.java
@@ -1,10 +1,13 @@
 package com.blog.webService.user;
 
+import com.blog.webService.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,10 +16,13 @@ public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
+
     @PostMapping("/api/1.0/users")
-    public void createUser(@RequestBody User user) {
-        userRepository.save(user);
+    public GenericResponse createUser(@RequestBody User user) {
+        userService.save(user);
         log.info(user.toString());
+
+        return new GenericResponse("User created.");
     }
 }
