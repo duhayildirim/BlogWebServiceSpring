c7e6ec5059fb391f28550bebb924ee3e61c89f1c
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-05-03 20:29:19 +0300

    Handle Validation Exception

--- src/main/java/com/blog/webService/user/UserController.java
+++ src/main/java/com/blog/webService/user/UserController.java
@@ -6,9 +6,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,28 +20,23 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/api/1.0/users")
-    public ResponseEntity<?> createUser(@Valid @RequestBody User user) {
+    public GenericResponse createUser(@Valid @RequestBody User user) {
+        userService.save(user);
+        return new GenericResponse("User created.");
+    }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleValidationException(MethodArgumentNotValidException exception) {
         ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
         Map<String, String> validationErrors = new HashMap<>();
-        String username = user.getUsername();
-        String email = user.getEmail();
 
-        if (username == null || username.isEmpty()) {
-            validationErrors.put("username", "Username cannot be null");
-        }
-        if (email == null || email.isEmpty()) {
-            validationErrors.put("email", "Email cannot be null");
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
 
-        if (validationErrors.size() > 0) {
-            error.setValidationErrors(validationErrors);
+        error.setValidationErrors(validationErrors);
 
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-        }
-
-        userService.save(user);
-
-        return ResponseEntity.ok(new GenericResponse("User created."));
+        return error;
     }
 }
