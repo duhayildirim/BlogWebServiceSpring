8f8869bec81b917939965dd5fbe268687877b50b
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-05-04 10:41:13 +0300

    bug fix

--- src/main/java/com/blog/webService/user/User.java
+++ src/main/java/com/blog/webService/user/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -23,10 +24,11 @@ public class User {
     private String username;
 
     @NotNull
+    @Email
     private String email;
 
     @NotNull
-    @Size(min = 6,max = 36)
+    @Size(min = 4)
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
 }
